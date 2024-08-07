b24fc29413d3bd7c740697c4a8f3084e07b79b6e
Harsha6152 <115444256+Harsha6152@users.noreply.github.com>
2024-08-07 15:49:46 +0530

    Add files via upload

---
+++ NODE1/Users.js
@@ -0,0 +1,24 @@
+const users = [
+    {
+        id: 1,
+        name: 'harsha',
+        email: 'harsha6152@gmail.com'
+    },
+    {
+        id: 2,
+        name: 'divya',
+        email: 'divya3952@gmail.com'
+    },
+    {
+        id: 3,
+        name: 'navya',
+        email: 'navya6252@gmail.com'
+    },
+    {
+        id: 4,
+        name: 'teju',
+        email: 'teju2452@gmail.com'
+
+    },
+]
+module.exports = users;
